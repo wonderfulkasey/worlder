c28fe4fb82b9e0c125aa650a0b0fc7f79c1cb6f4
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-08 23:44:59 -0400

    can add comment/char to worlds now

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,14 @@
 class CharactersController < ApplicationController
+
+    def create
+        @world = World.find(params[:world_id])
+        @character = @world.characters.create(character_params)
+        redirect_to world_path(@world)
+      end
+     
+      private
+        def character_params
+          params.require(:character).permit(:character, :body)
+        end
+
 end
