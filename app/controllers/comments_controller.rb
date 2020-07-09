d52f880aef4ee1b4d0bf115e55b268306988583e
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-09 00:40:52 -0400

    adding comments field to world

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+    def create
+        @world = World.find(params[:world_id])
+        @comment = @world.comments.create(comment_params)
+        redirect_to world_path(@world)
+      end
+     
+      private
+        def comment_params
+          params.require(:comment).permit(:commenter, :body)
+        end
 end
