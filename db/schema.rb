2db780a65343ee330750f9c9657e04911ceae3b5
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-09 01:44:38 -0400

    adding user views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_043515) do
+ActiveRecord::Schema.define(version: 2020_07_09_052816) do
 
   create_table "characters", force: :cascade do |t|
     t.string "character"
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 2020_07_09_043515) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["world_id"], name: "index_comments_on_world_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "worlds", force: :cascade do |t|
