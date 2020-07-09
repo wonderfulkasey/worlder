1d145e268febf15cc6250866f5a56289eb9e0ffb
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-08 23:07:44 -0400

    able to submit forms now

--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -1,8 +1,20 @@
 class WorldsController < ApplicationController
+   def show
+    @world = World.find(params[:id])
+   end
+
     def new
     end
 
     def create
-        render plain: params[:world].inspect
+        @world = World.new(world_params)
+ 
+        @world.save
+        redirect_to @world
+    end
+
+    private
+    def world_params
+        params.require(:world).permit(:title, :text)
     end
 end
