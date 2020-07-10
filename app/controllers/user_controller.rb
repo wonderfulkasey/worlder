9c106052d37e21ff3f29cf9a2f8b63258590f101
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-10 17:40:18 -0400

    redo

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,34 +1,16 @@
 class UserController < ApplicationController
-  before_action :logged_in?, only: [:index, :show]
-   
-    def index
-      render layout: 'login'
-    end
-  
-    def new
-      @user = User.new
-      render layout: 'login'
-    end
-  
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = @user.id # log in the user
-        redirect_to user_path(@user), flash: { success: "You successfully registered and created your preliminary profile, #{current_user.name}!" }
-      else
-        3.times { @user.treatments.build }
-        flash.now[:error] = "Your registration attempt was unsuccessful. Please try again."
-        render :new # present the registration form so the user can try signing up again
-      end
-  
-    end
-  
-  
-    ####################### END ROUTES #####################
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:username, :email_address, :password, :password_confirmation)
-    end
+  def show
+    @user = User.find_by(id: params[:id])
+        @worlds = @user.worlds
+        @characters = @user.characters
+  end
+
+  def create
+    User.create(user_params)
+  end
+ 
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
