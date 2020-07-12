8dd7a051047422be96ea2859b14f906e927b7e9b
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-12 13:05:15 -0400

    fixing sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-    has_many :worlds
+ has_many :worlds
     has_many :comments, through: :worlds
 
     validates :name, length: { minimum: 2 }
